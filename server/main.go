e4e1db2501df7a61e36d11ac3dbc822301a76690
tohrxyz <tomas.hrib@proton.me>
2024-08-29 18:21:32 +0200

    server: setup ws server

---
+++ server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow all origins for simplicity
+	},
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error while upgrading connection:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error while reading message:", err)
+			break
+		}
+		fmt.Printf("Received: %s\n", msg)
+
+		err = conn.WriteMessage(messageType, msg)
+		if err != nil {
+			fmt.Println("Error while writing message:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", wsEndpoint)
+	fmt.Println("Server started at :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
