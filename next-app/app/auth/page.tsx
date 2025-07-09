b96ca89ffca0707eb76d2212de7057837d65bdae
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-07-09 17:36:35 +0200

    feat(next): redirect from auth if logged-in

--- next-app/app/auth/page.tsx
+++ next-app/app/auth/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { useEffect } from "react";
 import Sign from "../components/Sign";
+import { getFromStorage } from "../lib/localStorage";
+import { LOCAL_STORAGE_KEYS } from "../constants/localStorageKeys";
+import { useRouter } from "next/navigation";
 
 export default function Auth() {
+    const router = useRouter();
+    useEffect(() => {
+        const user = getFromStorage(LOCAL_STORAGE_KEYS.USER);
+        if (user) {
+            router.push("/chat");
+        }
+    }, [router]);
+
     return (
         <div className="flex items-center justify-center min-h-screen">
             <Sign />
