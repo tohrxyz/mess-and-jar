66fd9c2373fb26ee878bb8b633db48eaeffb7fe6
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-07-14 20:06:06 +0200

    feat(next): add mutation for room ops

---
+++ next-app/app/mutations/room.ts
@@ -0,0 +1,42 @@
+import { getUserFromStorage } from "../lib/get-user-util";
+import { RoomBackendMethod, RoomCreateOrEditResponse, RoomGetResponse, RoomParams } from "../types";
+
+export const roomOperation = async ({ id, name, password, method }: RoomParams): Promise<RoomCreateOrEditResponse | RoomGetResponse> => {
+    const apiUrl = process.env.NEXT_PUBLIC_API_BACKEND_URL;
+    const user = getUserFromStorage()
+    if (!user || !user?.password || !user?.username) throw new Error("Can't get local user config")
+
+    const formData = new FormData();
+    formData.append("id", id);
+    formData.append("name", name || "");
+    formData.append("password", password || "");
+    formData.append("method", method)
+
+    const response = await fetch(`${apiUrl}/room`, {
+        method: "POST",
+        body: `id=${id}&name=${name}&password=${password}&method=${method}`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    });
+    if (!response.ok) {
+        return {
+            success: false,
+            message: response.statusText,
+        };
+    }
+    
+    if (method === RoomBackendMethod.RoomGet) {
+        const roomData = await response.json();
+        return {
+            success: true,
+            message: "Room retrieved successfully",
+            room: roomData
+        };
+    }
+    
+    return {
+        success: true,
+        message: "Room operation completed successfully",
+    };
+};
--- next-app/app/types/room.ts
+++ next-app/app/types/room.ts
@@ -3,3 +3,25 @@ export type Room = {
     name: string;
     password: string;
 }
+
+type DefaultRoomResponse = {
+    success: boolean;
+    message: string;
+}
+export type RoomCreateOrEditResponse = DefaultRoomResponse
+
+export type RoomGetResponse = DefaultRoomResponse & {
+    room: Room
+}
+export enum RoomBackendMethod {
+    RoomCreate = "create",
+    RoomEdit = "edit",
+    RoomGet = "get"
+}
+
+export type RoomParams = {
+    id: string,
+    name?: string,
+    password?: string,
+    method: RoomBackendMethod 
+}
