c9c40aa14b673553326be1b6b4c8a9c5a5738db6
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-12-25 20:20:25 +0100

    Add first iteration of backend switch to CF Workers and DO

---
+++ serverless/src/index.ts
@@ -0,0 +1,108 @@
+import { DurableObject } from "cloudflare:workers";
+import { createUser, editUser, getUser } from "./lib/auth";
+
+export class Chat extends DurableObject<Env> {
+	constructor(ctx: DurableObjectState, env: Env) {
+		ctx.storage.sql.exec(`
+			CREATE TABLE IF NOT EXISTS users (
+				username TEXT PRIMARY KEY,
+				password TEXT NOT NULL,
+				identity_pubkey TEXT
+			)
+		`)
+		super(ctx, env);
+	}
+	async sayHello(name: string): Promise<string> {
+		return `Hello, ${name}!`;
+	}
+
+	async auth(
+		username: string | undefined,
+		password: string | undefined,
+		identityPubkey: string | undefined
+	) {
+		if (!username) {
+			return Response.json({
+				status: 400,
+				error: "Username is required"
+			})
+		}
+		if (!password) {
+			return Response.json({
+				status: 400,
+				error: "Password is required"
+			})
+		}
+		if (!identityPubkey) {
+			return Response.json({
+				status: 400,
+				error: "Username is required"
+			})
+		}
+
+		try {
+			const user = getUser(username, this.ctx.storage.sql)
+			if (!user) {
+				createUser(
+					username,
+					password,
+					identityPubkey,
+					this.ctx.storage.sql
+				)
+				return Response.json({
+					status: 200,
+					message: "User created"
+				})
+			} else if (
+				user.identity_pubkey !== identityPubkey &&
+				(user.username !== "" && password === user.password)
+			) {
+				editUser(username, identityPubkey, this.ctx.storage.sql)
+				return Response.json({
+					status: 200,
+					message: "User edited"
+				})
+			}
+			else {
+				return Response.json({
+					status: 200,
+					message: "User with this username already exists"
+				})
+			}
+		} catch (error) {
+			return Response.json({
+				status: 500,
+				error: "Internal Server Error"
+			})
+		}
+	}
+}
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		const url = new URL(request.url)
+		const { pathname } = url
+		const stub = env.CHAT_DURABLE_OBJECT.getByName("chat")
+		switch (pathname) {
+			case "/auth":
+				if (request.method !== "POST") {
+					return Response.json({
+						status: 400,
+						error: "Method not allowed"
+					})
+				}
+				const formData = await request.formData()
+				const username = formData.get("username")?.toString()
+				const password = formData.get("password")?.toString()
+				const identityPubkey = formData.get("identity_pubkey")?.toString()
+				const authResponse = await stub.auth(username, password, identityPubkey)
+				return Response.json(await authResponse.json())
+			default:
+				const badRequest = {
+					status: 400,
+					error: "This route does not exist"
+				}
+				return new Response(JSON.stringify(badRequest))
+		}
+	},
+} satisfies ExportedHandler<Env>;
