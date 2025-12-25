e4d098de88371a7dc980a3a5911b070512304219
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-12-25 20:54:00 +0100

    feat(fe): new endpoint for auth and update next

--- next-app/app/mutations/auth.ts
+++ next-app/app/mutations/auth.ts
@@ -8,7 +8,7 @@ export const mutateAuth = async (
   password: string,
   identity_pubkey: string,
 ): Promise<AuthResponse> => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_BACKEND_URL
+  const apiUrl = process.env.NEXT_PUBLIC_API_AUTH_URL
   const response = await fetch(`${apiUrl}/auth`, {
     method: 'POST',
     body: `username=${username}&password=${password}&identity_pubkey=${identity_pubkey}`,
@@ -22,8 +22,11 @@ export const mutateAuth = async (
       message: response.statusText,
     }
   }
+
+  const body = await response.json()
+  console.log({body})
   return {
-    success: true,
-    message: response.statusText,
+    success: body.status,
+    message: body.message ?? body.error,
   }
 }
